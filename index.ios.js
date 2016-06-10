f650b5e2ee9b9b4f5aac7cba771615a467df2cb8
Jorgen Hookham <Jorgen@Hookham.co>
2016-06-10 10:26:32 +0300

    orange it up

--- index.ios.js
+++ index.ios.js
@@ -109,13 +109,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FFFFFF',
-  },
-  container2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFFFFF',
+    backgroundColor: '#FFBB33',
   },
   splashImage: {
     width: 320,
