598095d05609122180c3f68ad9575584321d737e
Jorgen Hookham <Jorgen@Hookham.co>
2016-06-11 10:39:25 +0300

    build out initialization flow, begin work on workout flow

--- index.ios.js
+++ index.ios.js
@@ -16,6 +16,13 @@ import {
 
 import Button from 'react-native-button'
 
+import {
+  Cell,
+  CustomCell,
+  Section,
+  TableView
+} from 'react-native-tableview-simple';
+
 const {
   workoutTemplatesJSON,
   workoutDataJSON
@@ -33,10 +40,10 @@ class W8M8 extends Component {
 
   componentDidMount () {
     var app = this;
-    this.requestWorkoutTemplates((request) => {
+    this.requestWorkoutTemplates((response) => {
       app.setState({
         stage: 2,
-        workoutTemplates: JSON.parse(request.responseText),
+        workoutTemplates: JSON.parse(response.responseText),
       });
     });
   }
@@ -51,28 +58,71 @@ class W8M8 extends Component {
     setTimeout(onload.bind(this, workoutTemplatesJSON), 1000);
   }
 
-  handleSetup (selectedTemplate) {
+  handleTemplateSelection (selectedTemplate) {
     this.setState({selectedTemplate: selectedTemplate})
   }
 
-  render() {
-    var splash = (this.state.stage == 1) ? <View style={styles.container}>
-      <Splash></Splash>
+  handleWorkoutCreation () {
+    this.setState({stage: 3});
+    var app = this;
+    var request = new XMLHttpRequest();
+    var params = `template_sheet_id=${this.state.selectedTemplate}`
+    request.onload = (response) => {
+      var d = JSON.parse(response.target.responseText);
+      this.setState({workoutSheetId: d});
+      this.getWorkoutData();
+    };
+    request.open('POST', 'http://w8m8.herokuapp.com/api/workouts/?format=json');
+    request.send(params);
+  }
+
+  getWorkoutData () {
+    var app = this;
+    var request = new XMLHttpRequest();
+    request.onload = (response) => {
+      var d = JSON.parse(response.currentTarget.responseText);
+      this.setState({stage: 4, summary: d.summary, steps: d.steps});
+    };
+    request.open('GET', `http://w8m8.herokuapp.com/api/workouts/${this.state.workoutSheetId}/?format=json`);
+    request.send();
+  }
+
+  handleWorkoutConfirmation () {
+    this.setState({stage: 5});
+  }
+
+  render () {
+    var splash = (this.state.stage == 1) ? <View style={styles.container} key={0}>
+      <SplashScreen />
     </View> : null;
-    var setup = (this.state.stage == 2) ? <View style={styles.container}>
-      <Setup {...this.state} onChange={this.handleSetup.bind(this)}></Setup>
+    var setup = (this.state.stage == 2) ? <View style={styles.container} key={1}>
+      <SetupScreen {...this.state}
+        handleTemplateSelection={this.handleTemplateSelection.bind(this)}
+        handleWorkoutCreation={this.handleWorkoutCreation.bind(this)}
+      />
     </View> : null;
+    var loading = (this.state.stage == 3) ? <View style={styles.container} key={2}>
+      <LoadingScreen message="Please wait while your workout log sheet is created." />
+    </View> : null;
+    var summary = (this.state.stage == 4) ? <View style={styles.container} key={3}>
+      <SummaryScreen {...this.state.summary}
+        handleWorkoutConfirmation={this.handleWorkoutConfirmation.bind(this)}
+      />
+    </View> : null;
+    var workoutFlow = (this.state.stage == 5) ? <View style={styles.container} key={4}>
+      <WorkoutFlow {...this.state.steps} appScope={this} />
+    </View> : null;
+    var screens = [splash, setup, loading, summary, workoutFlow];
     return (
       <View style={styles.container}>
-        {splash}
-        {setup}
+        {screens}
       </View>
     );
   }
 
 }
 
-class Splash extends Component {
+class SplashScreen extends Component {
   render () {
     return (
       <View style={styles.container}>
@@ -83,14 +133,16 @@ class Splash extends Component {
   }
 }
 
-class Setup extends Component {
+class SetupScreen extends Component {
 
   onChange (selectedTemplate) {
-    this.props.onChange(selectedTemplate)
+    this.props.handleTemplateSelection(selectedTemplate)
   }
 
   onPress (e) {
-    console.log('Pressed!');
+    if (this.props.selectedTemplate) {
+      this.props.handleWorkoutCreation();
+    }
   }
 
   render () {
@@ -110,6 +162,54 @@ class Setup extends Component {
       </View>
     );
   }
+}
+
+class LoadingScreen extends Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text>{this.props.message}</Text>
+        <Image style={styles.loader} source={require('./images/gears.gif')} resizeMode={'contain'}></Image>
+      </View>
+    );
+  }
+}
+
+class SummaryScreen extends Component {
+
+  handlePress (e) {
+    this.props.handleWorkoutConfirmation()
+  }
+
+  render () {
+    return (
+      <TableView>
+        <Section header="Workout Summary">
+          <Cell cellstyle="RightDetail" title="Mucle Groups" detail={this.props.muscle_groups} />
+          <Cell cellstyle="RightDetail" title="Reps" detail={this.props.rep_range} />
+          <Cell cellstyle="RightDetail" title="Speed" detail={this.props.speed} />
+          <Cell cellstyle="RightDetail" title="% of Max" detail={this.props.percent_of_max} />
+          <Cell cellstyle="RightDetail" title="Sets" detail={this.props.set_count} />
+          <Cell cellstyle="RightDetail" title="~ Duration" detail={this.props.approx_duration} />
+          <Cell cellstyle="RightDetail" title="Rest" detail={this.props.rest_time_range} />
+          <Cell cellstyle="RightDetail" title="Set Time" detail={this.props.set_time_range} />
+        </Section>
+      </TableView>
+      <Button style={styles.buttonActive} onpress={this.handlePress.bind(this)}>Confirm</Button>
+    );
+  }
+}
+
+class WorkoutFlow extends Component {
+  render () {}
+}
+
+class WorkoutExerciseScreen extends Component {
+  render () {}
+}
+
+class WorkoutRestScreen extends Component {
+  render () {}
 }
 
 const styles = StyleSheet.create({
